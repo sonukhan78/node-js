18c5bd6d0fca619977431b3b7b3ca093049f5b99
git config --global user.name islamkhan08 <i6960841@gmail.com>
2023-02-18 09:21:22 +0530

    third commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 // const url = 'mongodb://127.0.0.1:27017';
 console.log("===>", process.env.MONGO_URL);
 const Connectdb = async () => {
-  console.log("============hellooo")
+  // console.log("============hellooo")
+  console.log("sonu kha deshwali")
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
   } catch (error) {
